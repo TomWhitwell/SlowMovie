4a2650e5bdd73b701758c4dea4c23a30c70f8d1d
Rob Weber <robweberjr@gmail.com>
2021-01-09 16:52:45 -0600

    added support for new directories and updated file loading order

--- slowmovie.py
+++ slowmovie.py
@@ -13,13 +13,15 @@
 import os, time, sys, random
 from PIL import Image
 import ffmpeg
+import fnmatch
 import argparse
 
 # Ensure this is the correct import for your particular screen
 from waveshare_epd import epd7in5_V2 as epd_driver
 
 fileTypes = [".mp4", ".mkv"]
-
+HOME_DIR = os.path.dirname(os.path.realpath(__file__))
+NOW_PLAYING = os.path.join(HOME_DIR, 'nowPlaying')
 
 def generate_frame(in_filename, out_filename, time):
     try:
@@ -57,9 +59,11 @@ def check_mp4(value):
         raise argparse.ArgumentTypeError("%s should be an .mp4 file" % value)
     return value
 
-# Ensure this is the correct path to your video folder
-viddir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'Videos/')
-logdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'logs/')
+
+def check_dir(value):
+    if(not os.path.exists(value) and not os.path.isdir(value)):
+        raise argparse.ArgumentTypeError("%s is not a directory" % value)
+    return value
 
 
 parser = argparse.ArgumentParser(description='SlowMovie Settings')
@@ -67,6 +71,8 @@ parser.add_argument('-r', '--random', action='store_true',
     help="Random mode: chooses a random frame every refresh")
 parser.add_argument('-f', '--file', type=check_mp4,
     help="Add a filename to start playing a specific film. Otherwise will pick a random file, and will move to another film randomly afterwards.")
+parser.add_argument('-D', '--dir', type=check_dir,
+    help='Set the directory that contains the video files. Default is ./Videos/')
 parser.add_argument('-d', '--delay',  default=120,
     help="Delay between screen updates, in seconds")
 parser.add_argument('-i', '--inc',  default=4,
@@ -74,6 +80,15 @@ parser.add_argument('-i', '--inc',  default=4,
 parser.add_argument('-s', '--start',
     help="Start at a specific frame")
 args = parser.parse_args()
+
+# set paths to log and video directories
+viddir = os.path.join(HOME_DIR, 'Videos/')
+logdir = os.path.join(HOME_DIR, 'logs/')
+
+if(args.dir):
+    viddir = args.dir
+
+print("Video directory = %s" % viddir)
 
 frameDelay = float(args.delay)
 print("Frame Delay = %f" %frameDelay )
@@ -91,62 +106,52 @@ if args.file:
 else:
     print ("Continue playing existing file")
 
-# Scan through video folder until you find an .mp4 file
-currentVideo = ""
-videoTry = 0
-while not (currentVideo.endswith('.mp4')):
-    currentVideo = os.listdir(viddir)[videoTry]
-    videoTry = videoTry + 1
-
-# the nowPlaying file stores the current video file
-# if it exists and has a valid video, switch to that
-try:
-    f = open('nowPlaying')
-    for line in f:
-        currentVideo = line.strip()
-    f.close()
-except:
-    f = open('nowPlaying', 'w')
-    f.write(currentVideo)
-    f.close()
-
-videoExists = 0
-for file in os.listdir(viddir):
-    if file == currentVideo:
-        videoExists = 1
-
-if videoExists > 0:
-    print("The current video is %s" %currentVideo)
-elif videoExists == 0:
-    print('error')
-    currentVideo = os.listdir(viddir)[0]
-    f = open('nowPlaying', 'w')
-    f.write(currentVideo)
-    f.close()
-    print("The current video is %s" %currentVideo)
-
-movieList = []
-
-# log files store the current progress for all the videos available
-
-for file in os.listdir(viddir):
-    if not file.startswith('.'):
-        movieList.append(file)
-        try:
-            log = open(logdir +'%s<progress'%file)
-            log.close()
-        except:
-            log = open(logdir + '%s<progress' %file, "w")
-            log.write("0")
-            log.close()
-
+# get all mp4 files in video directory
+movieList = sorted(fnmatch.filter(os.listdir(viddir), '*.mp4'))
 print (movieList)
 
+# log files store the current progress for all the videos available
+for file in movieList:
+    try:
+        log = open(logdir +'%s<progress'%file)
+        log.close()
+    except:
+        log = open(logdir + '%s<progress' %file, "w")
+        log.write("0")
+        log.close()
+
+currentVideo = None
 if args.file:
     if args.file in movieList:
         currentVideo = args.file
     else:
-        print ('%s not found' %args.file)
+        print('Error loading given file %s' % args.file)
+else:
+    # the nowPlaying file stores the current video file
+    # if it exists and has a valid video, switch to that
+    try:
+        f = open(NOW_PLAYING)
+        for line in f:
+            currentVideo = line.strip()
+        f.close()
+        print('Found now playing file %s' % currentVideo)
+    except Exception as e:
+        print(e)
+        pass
+
+    if(currentVideo not in viddir):
+        currentVideo = None
+
+# if no video found, pick random
+if currentVideo is None:
+    currentVideo = movieList[0]
+
+try:
+    f = open(NOW_PLAYING, 'w')
+    f.write(currentVideo)
+    f.close()
+except:
+    pass
 
 print("The current video is %s" %currentVideo)
 
@@ -218,10 +223,9 @@ while 1:
     log.close()
 
 
-    f = open('nowPlaying', 'w')
+    f = open(NOW_PLAYING, 'w')
     f.write(currentVideo)
     f.close()
-
 
     epd.sleep()
     time.sleep(frameDelay)
