2d0993d927cb76bacbfd3a3496c3acc4d114f2a8
Rob Weber <robweberjr@gmail.com>
2021-04-07 13:57:40 -0500

    added note regarding default driver

--- slowmovie.py
+++ slowmovie.py
@@ -21,7 +21,7 @@ from fractions import Fraction
 frameIncrement = 4
 timeInterval = 120
 contrast = 1.0
-displayDriver = 'waveshare_epd.epd7in5_V2'
+displayDriver = 'waveshare_epd.epd7in5_V2'  # default display mirrors one referenced in README
 
 fileTypes = [".mp4", ".m4v", ".mkv"]
 
