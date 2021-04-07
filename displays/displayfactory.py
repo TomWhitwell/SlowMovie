0530fe72fe01bbd56ebba22eb9852fe7197d5cb8
Rob Weber <robweberjr@gmail.com>
2021-04-07 13:01:30 -0500

    dynamically load supported devices based on child classes

--- displays/displayfactory.py
+++ displays/displayfactory.py
@@ -1,9 +1,22 @@
+import importlib
+from . import VirtualDisplayDevice
 from . waveshare_display import WaveshareDisplay
 
 def list_supported_displays():
-    result = WaveshareDisplay.get_supported_devices()
+    result = []
 
-    return result
+    # get a list of display classes extending VirtualDisplayDevice
+    displayClasses = [(cls.__module__, cls.__name__) for cls in VirtualDisplayDevice.__subclasses__()]
+
+    for modName, className in displayClasses:
+        # load the module the class belongs to
+        mod = importlib.import_module(f"{modName}")
+        # get the class
+        classObj = getattr(mod, className)
+        # append supported devices of this class
+        result.append(classObj.get_supported_devices())
+
+    return sorted(result)
 
 def load_display_driver(displayName):
     deviceType = displayName.split('.')
