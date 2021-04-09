eec5a36c0ded8d2ff933ec2e867a2f0dfa0ed404
missionfloyd <missionfloyd@users.noreply.github.com>
2021-04-08 22:00:06 -0600

    Add default for directory argument
    Same thing, slightly fewer lines
    Fix increment help test

--- slowmovie.py
+++ slowmovie.py
@@ -185,9 +185,9 @@ parser = configargparse.ArgumentParser(default_config_files=["slowmovie.conf"])
 parser.add_argument("-f", "--file", type=check_vid, help="video file to start playing; otherwise play the first file in the videos directory")
 parser.add_argument("-R", "--random-file", action="store_true", help="play files in a random order; otherwise play them in directory order")
 parser.add_argument("-r", "--random-frames", action="store_true", help="choose a random frame every refresh")
-parser.add_argument("-D", "--directory", type=check_dir, help="directory containing available videos to play (default: %(default)s)")
+parser.add_argument("-D", "--directory", default=defaultDirectory, type=check_dir, help="directory containing available videos to play (default: %(default)s)")
 parser.add_argument("-d", "--delay", default=defaultDelay, type=int, help="delay in seconds between screen updates (default: %(default)s)")
-parser.add_argument("-i", "--increment", default=defaultIncrement, type=int, help=f"advance INCREMENT frames each refresh (default: {defaultDirectory})")
+parser.add_argument("-i", "--increment", default=defaultIncrement, type=int, help=f"advance INCREMENT frames each refresh (default: %(default)s)")
 parser.add_argument("-s", "--start", type=int, help="start playing at a specific frame")
 parser.add_argument("-c", "--contrast", default=defaultContrast, type=float, help="adjust image contrast (default: %(default)s)")
 parser.add_argument("-l", "--loop", action="store_true", help="loop a single video; otherwise play through the files in the videos directory")
@@ -195,10 +195,7 @@ parser.add_argument("--service", action="store_true", help=configargparse.SUPPRE
 args = parser.parse_args()
 
 # Set path of Videos directory and logs directory. Videos directory can be specified by CLI --directory
-if args.directory:
-    viddir = args.directory
-else:
-    viddir = defaultDirectory
+viddir = args.directory
 logdir = "logs"
 
 # Create logs and Videos directories if missing
